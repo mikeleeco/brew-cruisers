d923635fe5c1de49bc9336a24e73a9457d37809b
mikeleeco <mdlee12@gmail.com>
2025-05-05 16:59:13 -0500

    modify footer

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,43 +1,42 @@
 <script>
-	import ContainerLayout from "$components/ui/ContainerLayout.svelte";
-	import { games } from "$components/data/games";
-	import Icon from "@iconify/svelte";
-	import Seo from "$lib/components/utils/Seo.svelte";
+  import ContainerLayout from "$components/ui/ContainerLayout.svelte";
+  import { games } from "$components/data/games";
+  import Icon from "@iconify/svelte";
+  import Seo from "$lib/components/utils/Seo.svelte";
 </script>
 
 <Seo />
 <ContainerLayout>
-	<div slot="column">
-		<h1>Milwaukee Brew Cruisers Bike Ride</h1>
-		<h2 class="subtitle pb-5">
-			A monthly ride to enjoy live baseball and advocate for bicycle
-			accommodations in Milwaukee and the stadium district.
-		</h2>
-		<p>
-			Visit
-			<a href="/faq">FAQ</a> to learn more.
-		</p>
+  <div slot="column">
+    <h1>Milwaukee Brew Cruisers Bike Ride</h1>
+    <h2 class="subtitle pb-5">
+      A monthly ride to enjoy live baseball and advocate for bicycle
+      accommodations in Milwaukee and the stadium district.
+    </h2>
+    <p>
+      Visit
+      <a href="/faq">FAQ</a> to learn more.
+    </p>
 
-		<div>
-			<h2 class="text-primary font-serif pt-10">Ride Schedule</h2>
-			<ul class="">
-				{#each games as item}
-					<li class={`font-semibold p-2`}>
-						<span class="icon flex flex-row gap-2"
-							><Icon
-								icon={"mdi:baseball-diamond-outline"}
-								width="30"
-								height="30"
-							/>
-							<p class={`text-lg text-[${item.color}]`}>
-								<span class={`underline decoration-accent`}
-									>{item.name}</span
-								>, {item.date} @ {item.time}
-							</p>
-						</span>
-					</li>
-				{/each}
-			</ul>
-		</div>
-	</div>
+    <div>
+      <h2 class="text-primary font-serif pt-10">Ride Schedule</h2>
+      <ul class="">
+        {#each games as item}
+          <li class={`font-semibold p-2`}>
+            <span class="icon flex flex-row gap-2"
+              ><Icon
+                icon={"mdi:baseball-diamond-outline"}
+                width="30"
+                height="30"
+              />
+              <p class={`text-lg text-[${item.color}]`}>
+                <span class={`underline decoration-accent`}>{item.name}</span>, {item.date}
+                @ {item.time}
+              </p>
+            </span>
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
 </ContainerLayout>
